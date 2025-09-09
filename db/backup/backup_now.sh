d0bd2ca809c71759622bce4d8478047167db2732
Julian Lindsay-Kaufman <101271303+julianlk522@users.noreply.github.com>
2025-09-09 17:15:14 -0400

    update backup command (this should have been done when name was changed)

--- db/backup/backup_now.sh
+++ db/backup/backup_now.sh
@@ -3,5 +3,5 @@
 NOW=$(date -u +%m-%d-%y_%H:%M:%S)
 mkdir $NOW
 
-sqlite3 ../fitm.db ".backup '$NOW/fitm_$NOW.bak'"
-sqlite3 ../fitm.db .dump | gzip -c > $NOW/fitm_$NOW.dump.gz
+sqlite3 ../modeep.db ".backup '$NOW/modeep_$NOW.bak'"
+sqlite3 ../modeep.db .dump | gzip -c > $NOW/modeep_$NOW.dump.gz
